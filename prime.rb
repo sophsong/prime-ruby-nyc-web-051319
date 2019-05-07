fc833c6e74747a04a75183180ff4818f5a5a570d
Sophia Song <ososanne@gmail.com>
2019-05-07 01:45:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(number)
+  if number<= 1
+   
+    return false
+  end
+    
+ for n in 2..(number-1)
+ if number%n==0
+    return false
+  end
+    end
+    return true
+  end
+
+      
+      
