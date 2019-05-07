8101e9b5ef19a04a96c9f7d36821b09934672075
Sophia Song <ososanne@gmail.com>
2019-05-07 01:42:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(number)
+  if number<0 || number ==0|| number==log10
+    return false
+    
+  else
+    (2..number-1).to_a.all? do |factor|
+      number % factor ! = 0
+      
+      
+    end
+  end
+end
+      
+      
